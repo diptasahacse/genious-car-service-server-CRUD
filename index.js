ecdc9a9c296c518159081ea818f0864b33f176f8
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-29 02:00:28 +0600

    get all data and get specific data

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const ObjectId = require('mongodb').ObjectId;
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.onld4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        // connect to database
+        await client.connect();
+        // make a collection
+        const servicesCollection = client.db('geniousDB').collection('services');
+
+
+        // Get all services
+        app.get('/services', async (req, res) => {
+            // query
+            const query = {};
+            // cursor
+            const cursor = servicesCollection.find(query);
+            const allServices = await cursor.toArray()
+
+            // send all services
+            res.send(allServices)
+
+        })
+
+        // Get all services
+        app.get('/services/:id', async (req, res) => {
+            // get service id from client site
+            const serviceId = req.params.id;
+            // query
+            const query = { _id: ObjectId(serviceId) };
+
+            const singleService = await servicesCollection.findOne(query);
+
+
+            // send single service
+            res.send(singleService)
+
+        })
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Ready..')
+})
+// get All service
+app.get('/', (req, res) => {
+    res.send('Ready..')
+})
+
+
+
+app.listen(port, () => {
+    console.log("listening")
+})
